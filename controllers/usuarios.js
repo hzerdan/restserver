0b25b400b776aa0f0baa2e0adb9049ae28d975fe
Hugo <hzerdan@gmail.com>
2023-07-26 13:31:38 -0300

    112 parámetros de segmento y query

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,8 +1,13 @@
-const { response } = require('express');
+const { response, request } = require('express');
 
-const usuariosGet = (req, res = response) => {
+const usuariosGet = (req = request, res = response) => {
+    const {q, nombre="No name",apikey} = req.query;
+
     res.json({
-        msg: 'get API - controlador'
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey
     });
 }
 
@@ -17,8 +22,10 @@ const usuariosPost = (req, res = response) => {
 }
 
 const usuariosPut = (req, res = response) => {
+    const { id } = req.params
     res.json({
-        msg: 'put API - controlador'
+        msg: 'put API - controlador ',
+        id
     });
 }
 
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -8,7 +8,7 @@ const router = Router();
 
 // usuariosGet es la referencia. NO es la funcion usuariosGet()
 router.get('/',usuariosGet );
-router.put('/', usuariosPut);
+router.put('/:id', usuariosPut);
 router.post('/',usuariosPost);
 router.delete('/',usuariosDelete);
 router.patch('/',  usuariosPatch);
